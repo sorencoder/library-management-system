e6827e03beba2005c7f13780e12be35861bffe6c
sorencoder <69846992+sorencoder@users.noreply.github.com>
2022-10-12 18:04:28 +0530

    Add files via upload

---
+++ app/index.js
@@ -0,0 +1,107 @@
+console.log("This is index.js");
+// Todos"
+// 1. Store all the data to the localStorage
+// 2. Give another column as an option to delete the book
+// 3. Add a scroll bar to the view
+
+// Constructor
+function Book(bookName, author, studentName,enrollNo,date) {
+    this.bookName = bookname;
+    this.author = author;
+    this.studentName = studentName;
+    this.enrollNo = enrollNo;
+    this.date=date;
+}
+
+// Display Constructor
+function Display() {
+
+}
+
+// Add methods to display prototype
+Display.prototype.add = function (book) {
+    console.log("Adding to UI");
+    tableBody = document.getElementById('tableBody');
+    let uiString = `<tr>
+                        <td>${book.bookName}</td>
+                        <td>${book.author}</td>
+                        <td>${book.studentName}</td>
+                        <td>${book.enrollNo}</td>
+                        <td>${book.date}</td>
+                    </tr>`;
+    tableBody.innerHTML += uiString;
+}
+
+// Implement the clear function
+Display.prototype.clear = function () {
+    let libraryForm = document.getElementById('libraryForm');
+    libraryForm.reset();
+}
+
+// Implement the validate function
+Display.prototype.validate = function (book) {
+    if (book.bookName.length < 2 || book.author.length < 2) {
+        return false
+    }
+    else {
+        return true;
+    }
+}
+Display.prototype.show = function (type, displayMessage) {
+    let message = document.getElementById('message');
+    message.innerHTML = `<div class="alert alert-${type} alert-dismissible fade show" role="alert">
+                            <strong>Messge:</strong> ${displayMessage}
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                            </button>
+                        </div>`;
+    setTimeout(function () {
+        message.innerHTML = ''
+    }, 2000);
+
+}
+
+
+// Add submit event listener to libraryForm
+let libraryForm = document.getElementById('libraryForm');
+libraryForm.addEventListener('submit', libraryFormSubmit);
+
+function libraryFormSubmit(e) {
+    console.log('YOu have submitted library form');
+    let name = document.getElementById('bookName').value;
+    let author = document.getElementById('author').value;
+    let studentName = document.getElementById('student').value;
+    let enrollNo = document.getElementById('enroll').value;
+    let date = document.getElementById('issued').value;
+    let fiction = document.getElementById('fiction');
+    let programming = document.getElementById('programming');
+    let cooking = document.getElementById('cooking');
+
+    if (fiction.checked) {
+        type = fiction.value;
+    }
+    else if (programming.checked) {
+        type = programming.value;
+    }
+    else if (cooking.checked) {
+        type = cooking.value;
+    }
+
+    let book = new Book(bookName, author, studentName,enrollNo,date);
+    console.log(book);
+
+    let display = new Display();
+
+    if (display.validate(book)) {
+
+        display.add(book);
+        display.clear();
+        display.show('success', 'Your book has been successfully added')
+    }
+    else {
+        // Show error to the user
+        display.show('danger', 'Sorry you cannot add this book');
+    }
+
+    e.preventDefault();
+}
